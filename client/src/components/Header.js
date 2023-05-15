4fe974b31a317ab6b572ee8e37953d8067e594f1
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-05-15 11:19:09 +0530

    added navbar styling

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,8 +5,32 @@ function Header() {
     <Nav>
       <Logo src="/images/logo.svg"/>
       <NavMenu>
-
+          <a>
+            <img src="/images/home-icon.svg" />
+            <span>HOME</span>
+          </a>
+          <a>
+            <img src="/images/search-icon.svg" />
+            <span>SEARCH</span>
+          </a>
+          <a>
+            <img src="/images/watchlist-icon.svg" />
+            <span>WATCHLIST</span>
+          </a>
+          <a>
+            <img src="/images/original-icon.svg" />
+            <span>ORGINALS</span>
+          </a>
+          <a>
+            <img src="/images/movie-icon.svg" />
+            <span>MOVIES</span>
+          </a>
+          <a>
+            <img src="/images/series-icon.svg" />
+            <span>SERIES</span>
+          </a>
       </NavMenu>
+      <UserImg src="https://www.w3schools.com/howto/img_avatar.png"/>
     </Nav>
   )
 }
@@ -27,5 +51,30 @@ const Logo = styled.img`
 `
 
 const NavMenu = styled.div`
+  display: flex;
+  flex:1;
+  margin-left:25px;
+  align-items:center;
+  a{
+    display:flex;
+    align-items:center;
+    padding:0 12px;
+    cursor:pointer;
+
+    img{
+      height:20px;
+    }
+    span{
+      font-size:13px;
+      letter-spacing:1.42px;
+    }
+  }
+`
+
+const UserImg = styled.img`
+    width:48px;
+    height:48px;
+    border-radius:50%;
+    cursor:pointer;
 
 `
