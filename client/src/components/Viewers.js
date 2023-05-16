f59770e9ba3186d7e1036185f6fd8b90e823450f
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-05-16 13:29:14 +0530

    Added the cards styling

--- client/src/components/Viewers.js
+++ client/src/components/Viewers.js
@@ -39,11 +39,17 @@ const Wrap = styled.div`
     border: 3px solid rgba(249,249,249,0.1);
     box-shadow: rgb( 0 0 0 / 69%) 0px 26px 30px -10px,
     rgb(0 0 0 / 73%) 0px 16px 10px -10px;
-
+    cursor:pointer;
+    transition: all 250ms cubic-bezier(0.25,0.46,0.45,0.94) 0s;
     img{
         width:100%;
         height:100%;
         object-fit: cover;
+    }
+
+    &:hover{
+        transform: scale(1.05);
+        border-color: rgba(249,249,249,0.8);
     }
 
 `
