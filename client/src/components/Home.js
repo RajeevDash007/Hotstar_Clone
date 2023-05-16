47847e304bbfee9850b3529aa194cf6593e02066
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-05-16 11:35:16 +0530

    Added Carousel using React Slick

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
+import ImageSlider from './ImageSlider'
 
 function Home() {
   return (
     <Container>
-        Home
+        <ImageSlider /> 
     </Container>
   )
 }
@@ -16,6 +17,7 @@ const Container = styled.main`
   min-height: calc(100vh - 70px);
   padding: 0 calc(3.5vw + 5px);
   position:relative;
+  overflow-x:hidden;
 
   &:before{
     background: url("/images/home-background.png") center center / cover 
