30326fdee959407022c01b8b6fc2b88abe59c964
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-05-16 13:30:40 +0530

    Updated the pics

--- client/src/components/Viewers.js
+++ client/src/components/Viewers.js
@@ -7,16 +7,16 @@ function Viewers() {
         <img src="/images/viewers-disney.png" />
       </Wrap>
       <Wrap>
-        <img src="/images/viewers-disney.png" />
+        <img src="/images/viewers-pixar.png" />
       </Wrap>
       <Wrap>
-        <img src="/images/viewers-disney.png" />
+        <img src="/images/viewers-marvel.png" />
       </Wrap>
       <Wrap>
-        <img src="/images/viewers-disney.png" />
+        <img src="/images/viewers-starwars.png" />
       </Wrap>
       <Wrap>
-        <img src="/images/viewers-disney.png" />
+        <img src="/images/viewers-national.png" />
       </Wrap>
       
     </Container>
