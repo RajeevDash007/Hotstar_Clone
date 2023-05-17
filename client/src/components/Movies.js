4968cd544d3ee61226e25e1bfb8282671ef9d940
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-05-17 13:23:25 +0530

    added more cards in movies section

--- client/src/components/Movies.js
+++ client/src/components/Movies.js
@@ -18,6 +18,18 @@ function Movies() {
             <Wrap>
                 <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" />
             </Wrap>
+            <Wrap>
+                <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" />
+            </Wrap>
+            <Wrap>
+                <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" />
+            </Wrap>
+            <Wrap>
+                <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" />
+            </Wrap>
+            <Wrap>
+                <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" />
+            </Wrap>
         </Content>
     </Container>
   )
@@ -34,7 +46,7 @@ const Content = styled.div`
     display:grid;
     grid-gap: 25px;
     grid-template-columns: repeat(4,minmax(0,1fr));
-
+    overflow:hidden;
 
 `
 
