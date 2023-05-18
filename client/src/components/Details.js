15f99f9a8b5b4c8e1761589ad34c4448155c7c6b
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-05-18 16:42:53 +0530

    added Image Title

--- client/src/components/Details.js
+++ client/src/components/Details.js
@@ -7,6 +7,9 @@ function Details() {
       <Background>
         <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/4F39B7E16726ECF419DD7C49E011DD95099AA20A962B0B10AA1881A70661CE45/scale?width=1440&aspectRatio=1.78&format=jpeg" alt="" />
       </Background>
+      <ImageTitle>
+        <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/D7AEE1F05D10FC37C873176AAA26F777FC1B71E7A6563F36C6B1B497CAB1CEC2/scale?width=1440&aspectRatio=1.78" alt=""/>
+      </ImageTitle>
     </Container>
   )
 }
@@ -33,6 +36,20 @@ const Background = styled.div`
     width:100%;
     height:100%;
     object-fit:cover;
+  }
+
+`
+
+const ImageTitle = styled.div`
+  height:30vh;
+  min-height: 170px;
+  width:35vw;
+  min-width:200px;
+
+  img{
+    width:100%;
+    height:100%;
+    object-fit:contain;
   }
 
 `
