e25101a1351f85e0717de6f72f402b5aab49b707
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-05-18 11:46:37 +0530

    added details page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,28 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Home  from './components/Home';
+import Details from './components/Details';
+
+
 
 function App() {
   return (
     <div className="App">
-        <Header />
-        <Home />
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/detail/:id" element={<Details />} />
+      </Routes>
+    </Router>
+  </div>
   );
 }
 
