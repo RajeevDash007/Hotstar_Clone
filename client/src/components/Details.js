82d0b3949f085f29007a2c0e02f9b74a3cafc3e4
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-05-18 19:52:17 +0530

    added all buttons in details page

--- client/src/components/Details.js
+++ client/src/components/Details.js
@@ -10,6 +10,22 @@ function Details() {
       <ImageTitle>
         <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/D7AEE1F05D10FC37C873176AAA26F777FC1B71E7A6563F36C6B1B497CAB1CEC2/scale?width=1440&aspectRatio=1.78" alt=""/>
       </ImageTitle>
+      <Controls>
+        <PlayButton>
+          <img  src="/images/play-icon-black.png" alt="" />
+          <span>PLAY</span>
+        </PlayButton>
+        <TrailerButton>
+        <img  src="/images/play-icon-white.png" alt="" />
+          <span>Trailer</span>
+        </TrailerButton>
+        <AddButton>
+          <span>+</span>
+        </AddButton>
+        <GroupWatchButton>
+          <img src="/images/group-icon.png" />
+        </GroupWatchButton>
+      </Controls>
     </Container>
   )
 }
@@ -53,3 +69,56 @@ const ImageTitle = styled.div`
   }
 
 `
+
+const Controls = styled.div`
+  display:flex;
+  align-items: center;
+`
+
+const PlayButton = styled.button`
+  border-radius:4px;
+  font-size:15px;
+  display: flex;
+  align-items: center;
+  height:56px;
+  background: rgb(249,249,249);
+  border:none;
+  padding: 0px 24px;
+  margin-right:22px;
+  letter-spacing: 1.8px;
+  cursor:pointer;
+
+  &:hover{
+    background: rgb(198, 198, 198);
+  }
+`
+
+const TrailerButton = styled(PlayButton)`
+  background: rgba(0,0,0,0.3);
+  border: 1px solid rgb(249,249,249);
+  color:rgb(249,249,249);
+  text-transform: uppercase;
+`
+
+const AddButton = styled.button`
+  width: 44px;
+  height: 44px;
+  display: flex;
+  align-items:center;
+  justify-content:center;
+  border-radius:50%;
+  border: 2px solid white;
+  background-color: rgba(0,0,0,0.6);
+  cursor:pointer;
+  margin-right: 16px;
+
+  span{
+    font-size: 30px;
+    color: white;
+  }
+`
+
+const GroupWatchButton = styled(AddButton)`
+  background: rgb(0,0,0);
+`
+
