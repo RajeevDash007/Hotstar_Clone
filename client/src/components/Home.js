bb2d721d74f164cd8a4a6355e8b7e7a04e454ec4
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-05-20 04:57:29 +0530

    hotstar

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import styled from 'styled-components'
 import ImageSlider from './ImageSlider'
 import Viewers from './Viewers'
 import Movies from './Movies'
+import db from '../firebase'
 
 function Home() {
+  useEffect(()=>{
+    db.collection("movies").onSnapshot((snapshot)=>{
+      console.log(snapshot);
+    })
+  }, [])
   return (
     <Container>
         <ImageSlider /> 
---
+++ client/src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDxLx5FZTLb11y-KhVXoWz8-fJxXWvUM3E",
+  authDomain: "hotstar-clone-vj.firebaseapp.com",
+  projectId: "hotstar-clone-vj",
+  storageBucket: "hotstar-clone-vj.appspot.com",
+  messagingSenderId: "47609283656",
+  appId: "1:47609283656:web:2a0cca4e89417deaa2c47d",
+  measurementId: "G-C090P6HNT6"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage();
+
+export { auth, provider, storage };
+export default db ;
