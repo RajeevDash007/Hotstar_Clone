01589914ba3a22361f17e043ca813ee38f4eb3fc
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-05-19 10:58:02 +0530

    added background in login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Login() {
   return (
-    <div>
+    <Container>
       Login
-    </div>
+    </Container>
   )
 }
 
 export default Login
+
+const Container = styled.div`
+    position:relative;
+    height: calc(100vh - 70px);
+
+    &:before{
+        position:absolute;
+        top:0;
+        bottom:0;
+        left:0;
+        right:0;
+        content:"";
+        background-image: url("/images/login-background.jpg");
+        background-position:top;
+        background-size:cover;
+    }
+
+`
