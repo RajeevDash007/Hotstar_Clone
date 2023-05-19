f9e2362c9f6992d43f70e95923c31cac0a4ab7eb
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-05-19 11:32:14 +0530

    added login page fully

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,7 +4,16 @@ import styled from 'styled-components'
 function Login() {
   return (
     <Container>
-      Login
+      <CTA>
+        <CTALogoOne src="/images/cta-logo-one.svg" />
+        <SignUp>GET ALL THERE</SignUp>
+        <Description>
+            Get Premier Access to Raya and the Last Dragon for an additional fee
+            with a Disney+ subscription. As of 03/26/21, the price of Disney+
+            and The Disney Bundle will increase by $1.
+        </Description>
+        <CTALogoTwo src="/images/cta-logo-two.png" />
+      </CTA>
     </Container>
   )
 }
@@ -14,6 +23,10 @@ export default Login
 const Container = styled.div`
     position:relative;
     height: calc(100vh - 70px);
+    display:flex;
+    align-items:top;
+    justify-content:center;
+   
 
     &:before{
         position:absolute;
@@ -25,6 +38,53 @@ const Container = styled.div`
         background-image: url("/images/login-background.jpg");
         background-position:top;
         background-size:cover;
+        z-index:-1;
+        opacity:0.7;
     }
 
+`
+
+const CTA = styled.div`
+    max-width:650px;
+    padding:80px 40px;
+    width:90%;
+    display: flex;
+    flex-direction:column;
+    margin-top:50px;
+    align-items:center;
+`
+
+const CTALogoOne = styled.img`
+
+`
+
+const SignUp = styled.a`
+    width:100%;
+    background-color: #0063e5;
+    font-weight:bold;
+    padding:17px 0;
+    color: #f9f9f9;
+    border-radius: 4px;
+    text-align: center;
+    font-size: 18px;
+    cursor: pointer;
+    transition:all 250ms;
+    letter-spacing:1.5px;
+    margin-top:8px;
+    margin-bottom:12px;
+
+    &:hover{
+        background: #0483ee;
+    }
+`
+
+const Description = styled.p`
+    font-size: 11px;
+    letter-spacing: 1.5px;
+    text-align: center;
+    line-height: 1.5;
+`
+
+const CTALogoTwo = styled.img`
+    width:90%;
 `
